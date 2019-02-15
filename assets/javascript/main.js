dd2b8de9b20f3688ce5853214b3614a51c299e96
Timmy <123.tsa@gmail.com>
2019-02-15 15:31:36 -0500

    js initial map setup

---
+++ assets/javascript/main.js
@@ -0,0 +1,15 @@
+function initMap(){
+    // Map options
+    var options = {
+        zoom: 18,
+        center:{lat:38.8799,lng:-77.1067}
+    }
+    $(document).ready(function(){
+    //console.log(options.zoom);
+    // New map
+    var latitude;
+    var longitude;
+    var map = new google.maps.Map(document.getElementById('map'), options);
+    var pokemon = {};
+    )
+};
