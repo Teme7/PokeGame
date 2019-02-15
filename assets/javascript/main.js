625d052ec02abb3d2e2b095e8d10f25e94e64f41
Timmy <123.tsa@gmail.com>
2019-02-15 15:43:56 -0500

    map js

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -5,11 +5,19 @@ function initMap(){
         center:{lat:38.8799,lng:-77.1067}
     }
     $(document).ready(function(){
-    //console.log(options.zoom);
+    console.log(options.zoom);
     // New map
     var latitude;
     var longitude;
     var map = new google.maps.Map(document.getElementById('map'), options);
     var pokemon = {};
-    )
-};
+        
+    var coords = {};
+            
+    // Get the modal
+    var modal = document.getElementById('myModal');
+    
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+            
+    // Load pokemon.json file contents 
