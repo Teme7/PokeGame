cb4a06cf771bb78fff1ec6a70332b68775ba84ed
Timmy <123.tsa@gmail.com>
2019-02-19 19:42:40 -0500

    pokemon json

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -21,3 +21,87 @@ function initMap(){
     var span = document.getElementsByClassName("close")[0];
             
     // Load pokemon.json file contents 
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function(){
+        if(this.readyState == 4 && this.status == 200){
+            var response = JSON.parse(xhttp.responseText);
+            var item = response;
+            pokemon = item;
+                            
+        }
+        
+    };
+    
+    xhttp.open("GET", "pokemon.json", false);
+    xhttp.send();
+    
+   //Load worldCities.json file contents 
+    // var xhttp = new XMLHttpRequest();
+    // xhttp.onreadystatechange = function(){
+    //     if(this.readyState == 4 && this.status == 200){
+    //         var response = JSON.parse(xhttp.responseText);
+    //         var item = response;
+    //         coords = item;
+    //     }
+            
+    // };
+    
+    // xhttp.open("GET", "worldCities.json", false);
+    // xhttp.send();
+
+//       $.getJSON("pokemon.json", function(json){
+//           //pokemon.push(json);
+//       });
+    
+    
+//console.log(pokemon[0]);
+//console.log(coords[0]);
+    
+     
+  
+    google.maps.event.addListener(map, 'dragend', function(event) {
+    //var center = map.getCenter();
+        var mylat = map.getCenter().lat();
+        var mylng = map.getCenter().lng();
+        console.log(mylat);
+        console.log(mylng);
+        var yesNo = Math.floor((Math.random() * 2));
+        var pokemonNum = Math.floor((Math.random() * 35));
+        console.log(yesNo);
+        console.log(pokemon[pokemonNum].name);
+        if(yesNo){
+            addMarker(mylat, mylng, pokemonNum);
+        }
+    });
+               
+    function addMarker(lat, lng, num){
+        var name = pokemon[num].name;
+        //latitude = coords[latLong].lat;
+        //longitude = coords[latLong].lng;
+        var randomNumber = Math.floor(Math.random());
+        latitude = lat + randomNumber;
+        longitude = lng + randomNumber;
+        console.log(latitude);
+        console.log(longitude);
+        var randompoint = new google.maps.LatLng(latitude, longitude);
+        var marker = new google.maps.Marker({
+        position: randompoint,
+        map:map,
+        draggable: false,
+        animation: google.maps.Animation.BOUNCE,
+        
+    });
+        console.log(pokemon[num].image);
+    
+    //Set icon visibility according to zoom level 
+    google.maps.event.addListener(map, 'zoom_changed', function(event) {
+        var zoom = map.getZoom();
+        console.log(zoom);
+        if (zoom >= 17){
+            marker.setVisible(true);
+            marker.setAnimation(google.maps.Animation.BOUNCE);
+        } else {
+            marker.setVisible(false);
+        }
+     });
+        
